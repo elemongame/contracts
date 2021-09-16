68c5f38ff212f2f33fb98ebe1e6973eab1cb9f0e
elemongame <89892935+elemongame@users.noreply.github.com>
2021-09-16 21:03:35 +0700

    Update token ticker

--- contracts/ElemonToken.sol
+++ contracts/ElemonToken.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.7;
 import './ERC20.sol';
 
 contract ElemonToken is ERC20 {
-    constructor() ERC20("Elemon Token", "EMON", 500000000000000000000000000){}
+    constructor() ERC20("Elemon Token", "ELMON", 500000000000000000000000000){}
 }
 
 // SPDX-License-Identifier: MIT
